09d0b7fcb93440961801a430f5b620c6fc5d40ed
HadIhab <chocovani101@hotmail.fr>
2021-04-06 13:31:34 +0100

    Setting up debugging options

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
 const app = express();
+
+app.use(morgan('tiny'));
 
 app.get('/',function(req, res){
   res.send('Hello from my library app');
 });
 
 app.listen(3000,function(){
-  console.log('Listening on port 3000');
+  debug(`Listening on port 3000`);
 });
