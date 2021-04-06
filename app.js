ce8cc6b4a834c590c27ef7d2224e3cdb49c1fdbe
HadIhab <chocovani101@hotmail.fr>
2021-04-06 12:56:40 +0100

    Setting up express

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-express = require('express');
+const express = require('express');
+const app = express();
+
+app.get('/',function(req, res){
+  res.send('Hello from my library app');
+});
+
+app.listen(3000,function(){
+  console.log('Listening on port 3000');
+});
