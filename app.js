64b3fbf8adde94eed0ce25c1247131cc71f62fe4
HadIhab <chocovani101@hotmail.fr>
2021-04-08 18:55:20 +0100

    Setting up tooling

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname,'public/')));
+app.set('views','./src/views');
+app.set('view engine', 'pug');
 
 app.get('/',function(req, res){
   res.sendFile(path.join(__dirname,'views/index.html'));
 });
 
 app.listen(3000,function(){
-  debug(`Listening on port 3000`);
+  debug(`Listening on port ${chalk.red(port)}`);
 });
