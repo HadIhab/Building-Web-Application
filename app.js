fb2bea0eb76ba12a3022e7c2085d37726b2d2eab
HadIhab <chocovani101@hotmail.fr>
2021-04-09 21:06:55 +0100

    Fixing all the bootstrap css&js issues

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname,'public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views','./src/views');
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.get('/',function(req, res){
-  res.render('index', { title: 'My Library', list: ['a','b'] });
+  res.render('index', { title: 'My Library'});
 });
 
 app.listen(3000,function(){
