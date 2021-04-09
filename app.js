272101c3aaca8e0c22b0600b2be9482425ea7ace
HadIhab <chocovani101@hotmail.fr>
2021-04-09 22:44:50 +0100

    Testing a simple route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname,'public/')));
@@ -15,6 +16,11 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views','./src/views');
 app.set('view engine', 'ejs');
 
+bookRouter.route('/books')
+	.get((req,res)=>{
+		res.send('Hello books');
+	});
+app.use('/',bookRouter);	
 app.get('/',function(req, res){
   res.render(
   	'index', 
