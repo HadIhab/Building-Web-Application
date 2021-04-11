e968fed2096a4a46fbc339d3f2359992ea0cc7a2
HadIhab <chocovani101@hotmail.fr>
2021-04-11 14:04:17 +0100

    Creating MS Azure DB, and configurate it

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const sql = require('mssql');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const config = {
+    user: 'library',
+    password: 'SQLserver123',
+    server: 'ihab-library.database.windows.net',
+    database: 'Library',
+    "options": {
+        "encrypt": true,
+        "enableArithAbort": true
+        }
+};
+
+sql.connect(config).catch(err => debug(err));
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname,'public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
