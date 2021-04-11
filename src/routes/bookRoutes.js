4ecd6e82a5f812c81fd8675e55503f71ae7f5398
HadIhab <chocovani101@hotmail.fr>
2021-04-11 22:29:16 +0100

    Select One

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -103,7 +103,37 @@ function router(nav){
 	      }());
     });
 
+    bookRouter.route('/:id')
+    .get((req, res) => {
+	      const { id } = req.params;
+	      const url = 'mongodb://localhost:27017';
+	      const dbName = 'libraryApp';
 
+	      (async function mongo() {
+	        let client;
+	        try {
+	          client = await MongoClient.connect(url);
+	          debug('Connected correctly to server');
+
+	          const db = client.db(dbName);
+
+	          const col = await db.collection('books');
+
+	          const book = await col.findOne({ _id: new ObjectID(id) });
+	          debug(book);
+	          res.render(
+	            'bookView',
+	            {
+	              nav,
+	              title: 'Library',
+	              book
+	            }
+	          );
+	        } catch (err) {
+	          debug(err.stack);
+	        }
+	      }());
+    });
 
 	return bookRouter;	
 }
